8ce36e17ad0ebea9f24529d9042d1996b51a23f2
Woyengikuro Subiri <wsubiri@gmail.com>
2024-12-25 10:14:58 +0100

    fix calculator when operand is divided by 0

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function operation(num1, operand, num2){
   else if (symbol == "/"){
     operationResult = num1 / num2;
   }
+  operationResult = parseFloat(operationResult.toFixed(5));
 }
 
 
@@ -23,22 +24,19 @@ const equals = document.querySelector(".equals");
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 
-
+// Event listeners for clear and equals buttons
 clear.addEventListener("click", clearScreen);
 equals.addEventListener("click", operate);
 
-let symbol = null;
-let clickedNumber = '';
+let symbol = null, clickedNumber = '';
 let firstNumber = 0, secondNumber = 0, operationResult = 0;
 
 // Function to handle number button clicks
 function handleNumberClick(event) {
-  // firstNumber = operationResult;
   const currentDisplay = event.target.textContent;
   clickedNumber += currentDisplay;
   display.innerHTML = clickedNumber;
   let number = parseFloat(clickedNumber);
-  // alert(firstNumber)
   if (symbol === null) {
     firstNumber = number;
   }
@@ -52,18 +50,15 @@ function handleOperatorClick(event) {
   if (firstNumber && secondNumber) {
     operation(firstNumber, symbol, secondNumber);
     firstNumber = operationResult;
-    alert(operationResult);
-    display.innerHTML = (`${operationResult}`);
+    display.innerHTML = firstNumber;
   }
-  const currentDisplay = event.target.textContent;
   const operator = event.target.textContent;
-  let clickedOperator = currentDisplay;
-  display.innerHTML = clickedOperator;
+  let clickedOperator = operator;
   symbol = operator;
   clickedNumber = '';
 }
 
-function clearScreen(event){
+function clearScreen(){
   // display.innerHTML = "";
   clickedNumber = 0;
   display.innerHTML = clickedNumber;
@@ -75,15 +70,23 @@ numberButtons.forEach(button => {
 
 
 // perform the operation when equals sign is clicked
-function operate(event) {
-  // let number = event.target.textContent;
-  // display.innerHTML = number;
-  // alert(firstNumber);
-  // alert(symbol);
-  // alert(secondNumber);
-  operation(firstNumber, symbol, secondNumber);
-  display.innerHTML = (`${firstNumber} ${symbol} ${secondNumber} = ${operationResult}`);
-  firstNumber = operationResult;
+function operate() {
+  if (secondNumber === 0) {
+    if (!symbol) {
+      display.innerHTML = "ERROR!";
+    }
+    else if (symbol === '/') {
+      display.innerHTML = "THINK AGAIN!";
+    }
+    else {
+      display.innerHTML = "ERROR!"
+    }
+  }
+  else {
+    operation(firstNumber, symbol, secondNumber);
+    display.innerHTML = (`${firstNumber} ${symbol} ${secondNumber} = ${operationResult}`);
+    firstNumber = operationResult;
+  }
 }
 
 equals.addEventListener('click', operate);
